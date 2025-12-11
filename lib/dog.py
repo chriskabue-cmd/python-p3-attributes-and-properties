864c9a856c84b8040536253b7f9b89208d57a3d7
Chris kabue <chris.kabue@student.moringaschool.com>
2025-12-11 21:03:28 +0300

    phase3/python-p3-attributes-and-properties

--- lib/dog.py
+++ lib/dog.py
@@ -8,8 +8,26 @@ APPROVED_BREEDS = [
     "Beagle",
     "French Bulldog",
     "Pug",
-    "Pointer"
+    "Pointer",
+    "Mutt"
 ]
 
 class Dog:
+    def __init__(self, name="unknown", breed="Mutt"):
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+            self.name = name
+        else:
+            self.name = name
+        
+        if breed not in APPROVED_BREEDS:
+            print("Breed must be in list of approved breeds.")
+            self.breed = breed
+        else:
+            self.breed = breed
+         
+    def bark(self):
+            print("Woof!")
+    def sit(self):
+            print(f"{self.name} sits down.")
     pass
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,23 @@ APPROVED_JOBS = [
 ]
 
 class Person:
+    approved_jobs = ["Sales", "Manager", "Engineer", "Teacher", "Developer"]
+
+    def __init__(self, name="unknown", job=None):
+        # --- Validate name ---
+        if not isinstance(name, str) or not (1 <= len(name) <= 25):
+            print("Name must be string between 1 and 25 characters.")
+        else:
+            # convert name to title case before saving
+            name = name.title()
+
+        self.name = name
+
+        # --- Validate job only if provided ---
+        if job is not None:
+            if job not in self.approved_jobs:
+                print("Job must be in list of approved jobs.")
+            self.job = job
+        else:
+            self.job = None
     pass
